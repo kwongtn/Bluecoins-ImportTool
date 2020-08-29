3d6a8c3ca98a260143b105c00352a4fb0a533263
KwongTN <tungnan5636@gmail.com>
2020-08-29 11:29:34 +0800

    Shifted entry data struct to its own file
    Easier maintenance

--- BluecoinsImportTool.h
+++ BluecoinsImportTool.h
@@ -23,32 +23,7 @@ bool splitTransac = false;
 // Used to store all properties in the json file.
 json properties;
 
-// Used to store current entry before writing into file.
-struct ENTRY {
-	string type = "";			// Transaction category.
-	string transCat = "";		// Transaction parent.
-	string transChild = "";		// Transaction child.
-	string accCat = "";			// Account category.
-	string accChild = "";		// Account child.
-	unsigned short int		// Date & time.
-		year = 0,
-		month = 0,
-		day = 0,
-		hour = 1000,
-		mins = 1000;
-	double amount = 3.14159265359;
-	string title = "";			// Title of transaction.
-	string notes = "";
-	char status = '\0';
-	string label = "";
-
-	// Special variables for transfers
-	string sourceAccCat = "";
-	string sourceAccChild = "";
-	string destAccCat = "";
-	string destAccChild = "";
-};
-ENTRY entry;
+#include "./entryDataStruct.h"
 
 // Utility functions:
 
